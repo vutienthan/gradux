7d81f41ef55359f6bc4de21461636b2678cb751a
vutienthan <128622664+vutienthan@users.noreply.github.com>
2024-05-05 09:40:03 +0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,51 @@
+<html>
+  <head>
+    <title>menu</title>
+    <style>
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+header {
+    background-color: #f8f8f8;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+header a {
+    color: #007bff;
+    text-decoration: none;
+    font-size: 10px;
+}
+
+.logo {
+    font-size: 24px;
+    font-weight: bold;
+}
+
+nav ul {
+    list-style-type: none;
+    display: flex;
+}
+
+nav ul li {
+    margin-right: 20px;
+}
+    </style>
+  </head>
+  <body>
+<header>
+        <div class="logo">GRADUX</div>
+        <nav>
+            <ul>
+                <li><a href="./subjects.html">Предмет</a></li>
+                <li><a href="./dictionary.html">Словарь</a></li>
+                <li><a href="./program.html">Программирование</a></li>
+            </ul>
+        </nav>
+    </header>
+  </body>
+</html>
