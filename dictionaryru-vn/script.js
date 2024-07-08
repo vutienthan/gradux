bc0657a73af48ad06404a711d3c1df1b909e7d20
vutienthan <128622664+vutienthan@users.noreply.github.com>
2024-07-08 08:50:45 +0300

    Update script.js

--- dictionaryru-vn/script.js
+++ dictionaryru-vn/script.js
@@ -1,14 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('dictionary.json')
-        .then(response => response.json())
-        .then(dictionary => {
-            const wordList = document.getElementById('word-list');
-            dictionary.forEach(item => {
-                const wordItem = document.createElement('li');
-                wordItem.className = 'word-item';
-                wordItem.innerHTML = `<b>RU:</b> ${item.ru} <br> <b>VI:</b> ${item.vi}`;
-                wordList.appendChild(wordItem);
-            });
-        })
-        .catch(error => console.error('Error fetching dictionary:', error));
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav');
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('open');
+    nav.classList.toggle('open');
 });
