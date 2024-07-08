fd6285cb8c65e5ca074c8054a09b80afe7e4b4ad
vutienthan <128622664+vutienthan@users.noreply.github.com>
2024-07-08 08:21:14 +0300

    Update dictionary.js

--- dictionaryru-vn/dictionary.js
+++ dictionaryru-vn/dictionary.js
@@ -3,10 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(response => response.json())
         .then(dictionary => {
             const wordList = document.getElementById('word-list');
-            dictionary.forEach(item => {
+            dictionary.forEach((item, index) => {
                 const wordItem = document.createElement('li');
                 wordItem.className = 'word-item';
-                wordItem.innerHTML = `<b>RU:</b> ${item.ru} <br> <b>VI:</b> ${item.vi}`;
+                const container = document.createElement('div');
+                container.className = 'word-item-container';
+                container.innerHTML = `<b>${index + 1}. RU:</b> ${item.ru} <br> <b>VI:</b> ${item.vi}`;
+                wordItem.appendChild(container);
                 wordList.appendChild(wordItem);
             });
         })
