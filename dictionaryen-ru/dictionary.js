b776ac7a32f9941741349c916ff6b65cd260dd53
vutienthan <128622664+vutienthan@users.noreply.github.com>
2024-07-11 21:40:39 +0300

    Create dictionary.js

---
+++ dictionaryen-ru/dictionary.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('dictionary.json')
+        .then(response => response.json())
+        .then(dictionary => {
+            const wordList = document.getElementById('word-list');
+            dictionary.forEach(item => {
+                const wordItem = document.createElement('li');
+                wordItem.className = 'word-item';
+                wordItem.innerHTML = `${item.ru} <b>-</b> ${item.vi}`;
+                wordList.appendChild(wordItem);
+            });
+        })
+        .catch(error => console.error('Error fetching dictionary:', error));
+});
